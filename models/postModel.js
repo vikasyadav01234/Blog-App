c4cadc74281b702355278142dbd48d9c5cf1e26c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 11:45:13 +0530

    main

--- models/postModel.js
+++ models/postModel.js
@@ -1,2 +1,16 @@
 //import Mongoose
 const mongoose = require("mongoose");
+
+//route Handler
+
+//route handler
+const commentSchema = new mongoose.Schema({
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "POST", //reference to the post model
+    },
+    body: {
+        type:String,
+        required:true,
+    },
+});
