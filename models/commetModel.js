28e347d97ce92bfa2de44ffd30285f00f9a414c5
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 11:18:53 +0530

    main

--- models/commetModel.js
+++ models/commetModel.js
@@ -1,2 +1,9 @@
 //import Mongoose
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+//route handler
+const commentSchema new mongoose.Schema({
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+    }
+})
