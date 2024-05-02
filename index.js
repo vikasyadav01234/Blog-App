7b851c6c6f9df8b4f4386d8f1409bc15635c5be3
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 10:37:37 +0530

    main

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+//middleware
