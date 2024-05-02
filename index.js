bb09c76bd16ad5eac101f8ad6d1b133c0ed76ec3
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 10:37:14 +0530

    main

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("App is Running Successfully")
-})
-
-app.get("/", (req,res)=> {
-    res.send(`<h1>This is My Home</h1>`)
-})
+require("dotenv").config();
+const PORT = process.env.PORT || 3000
