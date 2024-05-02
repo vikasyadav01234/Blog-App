c39128dec4c07ba649c20e46fa15f2a6c8f7fc79
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 11:47:13 +0530

    main

--- models/postModel.js
+++ models/postModel.js
@@ -8,10 +8,11 @@ const commentSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-    }
-    
+    },
+
     body: {
         type:String,
         required:true,
     },
+    
 });
