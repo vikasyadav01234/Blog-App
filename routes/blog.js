4e38ef5f15e44a98c868c1df31e6ed95a54bfe22
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-05-02 12:09:03 +0530

    main

--- routes/blog.js
+++ routes/blog.js
@@ -6,9 +6,10 @@ const {dummyLink} = require("../controllers/LikeController");
 
 
 //Mapping Create
-router.get("/dummyroute", dummyLink)
+router.get("/dummyroute", dummyLink);
 
 
 
 
 //export
+module.exports = router;
